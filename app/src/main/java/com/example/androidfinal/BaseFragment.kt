e25a131010ac2fe11f0b0a9822429a8a92688a6d
nadavaza <nadavaza@gmail.com>
2025-03-17 19:14:06 +0200

    add base Fragment

---
+++ app/src/main/java/com/example/androidfinal/BaseFragment.kt
@@ -0,0 +1,11 @@
+package com.example.androidfinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class BaseFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
